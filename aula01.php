2459e09ee293a8d64e7cf072c8beb303fd48a7cf
Tiago Da Silva <tiagopdasgithub@gmail.com>
2023-07-14 00:06:13 -0400

    Create aula01.php

---
+++ aula01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+        echo 'Hello World!</br>';
+    /*string */
+        $nome = 'Tiago';
+        echo 'Meu nome é: ';
+        echo $nome;
+        $idade = 33;
+        echo '</br>Minha idade é: ';
+        echo $idade;
+
+        $nome = 'Rubia';
+        echo '</br>';
+        echo 'Meu nome é: ';
+        echo $nome;
+        ?>
+    </p>
+</body>
+</html>
